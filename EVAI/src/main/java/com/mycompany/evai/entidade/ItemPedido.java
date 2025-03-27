f90d44437cc5f9dc15bf4aae05f69768bfe5898b
Amanda <amandaffangulo@gmail.com>
2025-03-27 08:26:45 -0300

    criação da classe ItemPedido

---
+++ EVAI/src/main/java/com/mycompany/evai/entidade/ItemPedido.java
@@ -0,0 +1,64 @@
+package com.mycompany.evai.entidade;
+
+public class ItemPedido {
+    private int id;
+    private Produto produto;
+    private int quantidade;
+    private double valorUnitario;
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the produto
+     */
+    public Produto getProduto() {
+        return produto;
+    }
+
+    /**
+     * @param produto the produto to set
+     */
+    public void setProduto(Produto produto) {
+        this.produto = produto;
+    }
+
+    /**
+     * @return the quantidade
+     */
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    /**
+     * @param quantidade the quantidade to set
+     */
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    /**
+     * @return the valorUnitario
+     */
+    public double getValorUnitario() {
+        return valorUnitario;
+    }
+
+    /**
+     * @param valorUnitario the valorUnitario to set
+     */
+    public void setValorUnitario(double valorUnitario) {
+        this.valorUnitario = valorUnitario;
+    }
+}
